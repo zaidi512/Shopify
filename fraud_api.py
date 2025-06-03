00c0a38207830e5d587be9e538f8afbd8b622a83
zaidi512 <45984783+zaidi512@users.noreply.github.com>
2025-06-03 22:21:08 +0500

    Update fraud_api.py

--- fraud_api.py
+++ fraud_api.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
